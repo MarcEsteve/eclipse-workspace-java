4109bafd3483afb2daedc8aa464387b80a4a3012
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-06-13 11:45:09 +0200

    Ejercicio pagina 380 resuelto

---
+++ HelloWorld/src/MainCoche.java
@@ -0,0 +1,26 @@
+
+public class MainCoche {
+
+	public static void main(String[] args) {
+		Coche honda = new Coche(3);
+		Coche opel = new Coche(105.0f,19.0f);
+		Coche audi = new Coche(0.0f , 350.0f , 2, 0);
+		//invocación de los métodos estáticos
+		//Primer método le pasamos 350CV y nos convierte a 259.26Kw
+		float kw = Coche.convertirCvKw(350.0f);
+		//Primer método le pasamos 259.26Kw y nos convierte a 350CV
+		float cv = Coche.convertirKwCv(kw);
+		
+		//Mostrar el resultado de los dos cálculos de los métodos static
+		//System.out.println(kw); //259.26Kw
+		//System.out.println(cv); //350CV
+		
+		//También se puede llamar al método estatico desde un objeto
+		//audi.convertirCvKw(350.0f);
+		//No necesita invocar a través de un Objeto
+		System.out.println(Coche.factorConversionCvKw);
+		Coche.factorConversionCvKw = 2.36f;
+		System.out.println(Coche.factorConversionCvKw);
+	}
+
+}
