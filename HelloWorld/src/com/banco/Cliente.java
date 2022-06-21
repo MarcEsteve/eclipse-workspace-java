3cfe4f5051aa3deca11bd269dfa94d1efa96aa8e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-06-21 12:29:02 +0200

    Gestor.java y Cliente.java

--- HelloWorld/src/com/banco/Cliente.java
+++ HelloWorld/src/com/banco/Cliente.java
@@ -1,24 +1,34 @@
 package com.banco;
 
-public class Gestor {
+public class Cliente {
 	//Constructores
-	public Gestor() {
+	public Cliente() {
 		this.id=1;
+		this.id_gestor=1;
 		this.usuario="Marc";
 		this.password="12345";
 		this.correo="marc@correo.com";
+		this.saldo=2000.0;
 	}
 	//Atributos
 	private int id;
+	private int id_gestor;
 	private String usuario;
 	private String password;
 	private String correo;
+	private double saldo;
 	//Métodos "getters" y "setters"
 	public int getId() {
 		return id;
 	};
 	public void setId(int id) {
 		this.id = id;
+	};
+	public int getIdGestor() {
+		return id_gestor;
+	};
+	public void setIdGestor(int id_gestor) {
+		this.id_gestor = id_gestor;
 	};
 	public String getUsuario() {
 		return usuario;
@@ -37,5 +47,11 @@ public class Gestor {
 	};
 	public void setCorreo(String correo) {
 		this.correo = correo;
+	};
+	public double getSaldo() {
+		return saldo;
+	};
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
 	};
 }
